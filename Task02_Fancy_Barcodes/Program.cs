549af8db7715ce4a87159df4cc98d572994c79de
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-22 23:10:37 +0200

    Add files via upload

---
+++ Task02_Fancy_Barcodes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task02_Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string patern = @"(@#+)([A-Z][\d|[a-zA-Z)]{4,}[A-Z])(@#+)";   // @"(@#+)([A-Z][\d|[A-z)]{4,}[A-Z])(@#+)";   //@"(@#+)([A-Z][[[:alnum:]]{4,}[A-Z])(@#+)";
+            Regex validBarCode = new Regex(patern);
+
+
+            int productsNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= productsNumber; i++)
+            {
+                string productGroup = string.Empty;
+                string nextProduct = Console.ReadLine();
+
+                MatchCollection productBarCode = validBarCode.Matches(nextProduct);
+
+                if (productBarCode.Count > 0)
+                {
+                    string helpName = productBarCode[0].ToString();
+
+                    //string helpName = productBarCode.ToString();
+
+                    string digitPatern = @"\d";
+                    Regex extractDigits = new Regex(digitPatern);
+
+                    MatchCollection digits = extractDigits.Matches(helpName);
+
+                    foreach (Match digit in digits)
+                    {
+                        productGroup += digit;
+                    }
+
+                    if(productGroup.Length < 1)
+                    {
+                        productGroup = "00";
+                    }
+
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                    //break;
+                }
+            }
+        }
+    }
+}
